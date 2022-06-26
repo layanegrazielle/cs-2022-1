7757d13644cf9955ab766acc069860dacb5cdd21
Layane Grazielle <81783870+layanegrazielle@users.noreply.github.com>
2022-06-26 13:07:07 -0300

    Update Exercicio3corrigido.java

--- tarefa006/Exercicio3corrigido.java
+++ tarefa006/Exercicio3corrigido.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Exercicio3 {
+public class Exercicio3corrigido {
     public static void main(String[] args) {
         double saldo;
         try (Scanner ler = new Scanner(System.in)) {

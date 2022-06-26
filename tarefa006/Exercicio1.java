65957f725c0d20d112ef30118ef4b2c152d273e6
Layane Grazielle <81783870+layanegrazielle@users.noreply.github.com>
2022-06-26 02:14:32 -0300

    Correção exercício 1: restrições nos valores das entradas

--- tarefa006/Exercicio1.java
+++ tarefa006/Exercicio1.java
@@ -7,7 +7,12 @@ public class Exercicio1{
         anos = ler.nextInt();
         meses = ler.nextInt();
         dias = ler.nextInt();
+        if(anos > -1 && meses < 13 && dias < 31) {
        total = anos * 365 + meses * 30 + dias;
 		System.out.println(total);
+        }
+        else{
+            System.out.println("Formatos aceitos:\nAnos >=0\nMeses <= 12\nDias <= 30");
+        }
 	}
 }
--- tarefa006/Exercicio1corrigido.java
+++ tarefa006/Exercicio1corrigido.java
@@ -8,7 +8,12 @@ public class Exercicio1corrigido{
         meses = ler.nextInt();
         dias = ler.nextInt();
     }
+        if(anos > -1 && meses < 13 && dias < 31) {
        total = anos * 365 + meses * 30 + dias;
 		System.out.println(total);
+        }
+        else{
+            System.out.println("Formatos aceitos:\nAnos >=0\nMeses <= 12\nDias <= 30");
+        }
 	}
 }
